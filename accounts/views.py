39ce8b157b7f836ecb746623c2a321cf9f75f35d
Noah-marc <104438947+Noah-marc@users.noreply.github.com>
2022-06-03 10:39:58 +0200

    Introducing login form

--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout 
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # maybe use include 
 def login_view(request):
     if request.method == "POST": 
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        user = authenticate(request, username = username, password = password)
-        if user is None: 
-            context = {"error": "Invalid username or password"}
-            return render (request, "accounts/login.html" , context)
-        login(request, user)
-        return redirect("/profiles/") #TASK: find out how to redirect to 'profiles/details/<int:id>/'
-    return render (request, "accounts/login.html" , {})
+        form = AuthenticationForm(request, data = request.POST)
+        if form.is_valid: 
+            user = form.get_user
+            login(request, user)
+            return redirect("") #TASK: find out how to redirect to 'profiles/details/<int:id>/'
+    else: 
+        form = AuthenticationForm(request)
+    context = {"form" : form}
+    return render (request, "accounts/login.html" , context)
 
 
 def logout_view(request):

318dfa5390c2770f8fe8a3c959f0544fc7d10eb4
Ramazan Emre Akdag <emreakdag03@hotmail.com>
2022-06-09 12:00:01 +0200

    Revert "cringe"
    This reverts commit 5875a45c63b77e5a65386a8e56dfd409eb456e5c.

--- datastructures/views.py
+++ datastructures/views.py
@@ -35,12 +35,9 @@ def profile_search_view(request):
 def profile_create_view(request): 
     context = {}
     if request.method == "POST": 
-        # user = User.objects.get(request.user_id) # van int -> user
-        user = request.user
-        # firstName = request.user.first_name
-        # lastName = request.user.last_name
-        firstName = request.POST.get("First Name")
-        lastName = request.POST.get("Last Name")
+        user = User.objects.get(request.user_id) # van int -> user
+        firstName = request.user.first_name
+        lastName = request.user.last_name
         studentNumber = request.POST.get("Student Number")
         studyProgram = request.POST.get("Study program")
         gender = request.POST.get("Gender")
@@ -88,14 +85,14 @@ def groups_create_view(request):
         groupName = request.POST.get("Group Name")
         course = request.POST.get("Group Course")
         groupSize = request.POST.get("Group Size")
-        group = Group.objects.create(groupName = groupName, course = course, groupSize = groupSize)
-        Profile.user = request.user
+        group = Group.objects.create(groupName = groupName, course = course, groupSize = groupSize,)
+        Profile.user = request.user 
         profile = get_object_or_404(Profile, pk=Profile.user.id)
         group_joined = True
         owner = Membership.objects.create(profile = profile, group = group, group_joined = group_joined)
         context = {
             "group_obj" : group,
-            "owner_obj" : profile,
+            "owner_obj" : owner,
             "created" : True
         } 
         return redirect("/")
